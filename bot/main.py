3f5d94b72f7296253b8d7d1f57c9eebc58a49000
Dylan Corrales <DeathCamel57@gmail.com>
2021-02-12 11:04:03 -0500

    Update .gitignore, main.py, and 2 more files...

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,75 @@
-import os
-from discord.ext import commands
+import discord
+import time
+import random
+import asyncio
 
-bot = commands.Bot(command_prefix="!")
-TOKEN = os.getenv("DISCORD_TOKEN")
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.members = True
 
-@bot.event
+client = discord.Client(intents=intents)
+usernames = []
+
+# Add users with errors to blacklist so that we don't keep making API calls to change these users.
+memberBlacklist = []
+
+file = open('usernames.txt', 'r')
+for line in file:
+    usernames.append(line[:-1])
+
+
+@client.event
 async def on_ready():
-    print(f"Logged in as {bot.user.name}({bot.user.id})")
+    print('We have logged in as {0.user}'.format(client))
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("pong")
+    while True:
+        users = client.get_all_members()
+        for member in users:
+            if member.display_name not in memberBlacklist:
+                await changeNick(member)
+            else:
+                print('Not modifying ' + member.display_name + ', as we didn\'t have permission to last time.')
+        time.sleep(5)
 
-if __name__ == "__main__":
-    bot.run(TOKEN)
+
+def generateusername():
+    newUsername = ""
+    usernameSeed = usernames[random.randint(0, len(usernames))]
+
+    # Determine where numbers occur
+    # 0 = None; 1 = Before; 2 = After; 3 = Both
+    randomInt = random.randint(0, 3)
+    if randomInt == 0:
+        newUsername += usernameSeed
+    if randomInt == 1:
+        for i in range(0, random.randrange(0, 6)):
+            newUsername += str(random.randrange(0, 9))
+        newUsername += usernameSeed
+    if randomInt == 2:
+        newUsername += usernameSeed
+        for i in range(0, random.randrange(0, 6)):
+            newUsername += str(random.randrange(0, 9))
+    if randomInt == 3:
+        for i in range(0, random.randrange(0, 6)):
+            newUsername += str(random.randrange(0, 9))
+        newUsername += usernameSeed
+        for i in range(0, random.randrange(0, 6)):
+            newUsername += str(random.randrange(0, 9))
+    return newUsername
+
+
+async def changeNick(member):
+    print("Changing nickname for " + member.display_name)
+    newname = generateusername()
+    try:
+        await member.edit(nick=newname)
+    except:
+        print('    Couldn\'t edit member ' + member.display_name)
+        print('    Adding member to blacklist...')
+        memberBlacklist.append(member.display_name)
+
+    print("    Changed username to " + newname)
+
+
+client.run('ODA5NzkzMzczMDg2OTQxMjE0.YCaQ2Q.Ljnlq26bnaotug8Zuwnx-whzdjE')
